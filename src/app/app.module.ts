f03870191a8ebd3450e80c771fa606d63b85d8b4
emersonvergara <emerson.vergara@ceiba.com.co>
2024-11-03 20:26:26 -0500

    Commit: Config shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
-import { BreadcrumbComponent } from './shared/components/breadcrumb/breadcrumb.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-
+import { SharedModule } from './shared/shared.module';
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbComponent,
-    FooterComponent,
-  ],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })

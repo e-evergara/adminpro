d8a17341910e59068114b9ea3a158c889ecf1355
emersonvergara <emerson.vergara@ceiba.com.co>
2024-11-04 15:14:24 -0500

    commit: refactor en el diseño

--- src/app/feature/admin/admin.module.ts
+++ src/app/feature/admin/admin.module.ts
@@ -4,9 +4,19 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { BreadcrumbComponent } from './shared/components/breadcrumb/breadcrumb.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [
+    AdminComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbComponent,
+    FooterComponent,
+  ],
   exports: [AdminComponent],
   imports: [CommonModule, AdminRoutingModule, SharedModule],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbComponent,
-    FooterComponent,
-  ],
-  exports: [
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbComponent,
-    FooterComponent,
-  ],
+  declarations: [],
+  exports: [],
   imports: [CommonModule],
 })
 export class SharedModule {}
